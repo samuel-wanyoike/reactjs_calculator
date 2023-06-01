b2c8ed494b62add22de232b67501f5a2f0e44fb5
samuel-wanyoike <125296142+samuel-wanyoike@users.noreply.github.com>
2023-06-01 21:45:15 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Display } from './components/display';
+import { Keyboard } from './components/keyboard';
 
 function App() {
+
+
+  const operators = ['AC', '/', 'X', '+', '-', '='];
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const [input, setInput] = useState(0);
+  const [output, setOutput] = useState('');
+  const [calculatorData, setCalculatorData] = useState('');
+
+  const handleInput = (value) => {
+    console.log(value);
+  };
+
+  const handleOutput = () => {
+    setOutput(calculatorData);
+  };
+
+  useEffect(() => {
+    handleOutput()
+  }, [calculatorData]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='calculator'>
+        <Display input={input} output={output}/>
+        <Keyboard handleInput={handleInput}/>
+      </div>
     </div>
   );
 }
