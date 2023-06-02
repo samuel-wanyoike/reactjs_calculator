30dac115e8241c59969b95ad2bca9bf9582050e5
samuel-wanyoike <125296142+samuel-wanyoike@users.noreply.github.com>
2023-06-03 01:47:45 +0300

    finalized the handle input functions

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Keyboard } from './components/keyboard';
 function App() {
 
 
-  const operators = ['AC', '/', 'X', '+', '-', '='];
+  const operators = ['AC', '/', 'x', '+', '-', '='];
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
   const [input, setInput] = useState(0);
@@ -18,17 +18,91 @@ function App() {
   //functions in the switch cases
 
   const handleSubmit = () => {
-    console.log('handlesubmit')
+    const total = eval(calculatorData);
+    setInput(`${total}`);
+    setOutput(`${total}`);
+    setCalculatorData(`${total}`);
   };
 
-  const handleClear = () => {};
+  const handleClear = () => {
+    setOutput('');
+    setInput('0');
+    setCalculatorData('');
+  };
 
-  const handleNumbers = () => {};
+  const handleNumbers = (value) => {
 
-  const dotOperator = () => {};
+    if (!calculatorData.length){
+      setInput(`${value}`);
+      setCalculatorData(`${value}`);
+    }
+    else {
+      if(value === 0 && (calculatorData=== '0' || input=== '0')){
+        setCalculatorData(`${calculatorData}`)
+      }
+      else {
+        const lastChat = calculatorData.charAt(calculatorData.length - 1);
+        const isLastChatOperator = lastChat === '*' || operators.includes(lastChat);
 
-  const handleOperators = () => {};
+        setInput(isLastChatOperator ? `${value}` : `${input}${value}` );
+        setCalculatorData(`${calculatorData}${value}`);
 
+      }
+    }
+
+  };
+
+  const dotOperator = () => {
+    const lastChat = calculatorData.charAt(calculatorData-1);
+    if(!calculatorData.length){
+      setInput('0.');
+      setCalculatorData('0.');
+    }
+    else{
+      if(lastChat === '*' || operators.includes(lastChat)){
+        setInput('0.');
+        setCalculatorData(`${calculatorData}0.`);
+      }
+      else{
+        setInput(
+          lastChat === '.' || input.includes('.') ? `${input}` : `${input}.`
+        );
+        setCalculatorData(
+          lastChat === '.' || input.includes('.') ? `${calculatorData}` : `${calculatorData}.`
+        )
+      }
+    }
+  };   
+
+  const handleOperators = (value) => {
+    if(calculatorData){
+      setInput(`${value}`);
+
+      const beforeLastChat = calculatorData.charAt(calculatorData-2);
+      const beforeLastChatIsOperator = operators.includes(beforeLastChat) || beforeLastChat === '*';
+      const lastChat = calculatorData.charAt(calculatorData-1);
+      const lastChatIsOperator = operators.includes(lastChat) || lastChat === '*';
+
+      const validOperator = value === 'x' ? '*' : value;
+
+      if(
+        (lastChatIsOperator && value !== '-') || beforeLastChatIsOperator && lastChatIsOperator){
+          if(beforeLastChatIsOperator){
+            const updatedValue = `${calculatorData.substring(0, calculatorData-2)}${validOperator}`;
+            setCalculatorData(updatedValue)
+          }
+          else{
+            setCalculatorData(`${calculatorData.substring(0, calculatorData-1)}${validOperator}`)
+          }
+      }
+      else{
+        setCalculatorData(`${calculatorData}${validOperator}`);
+      }
+      
+    }
+  };
+
+  //handleInput is called when any key is clicked
   const handleInput = (value) => {
     const number = numbers.find((num) => num === value);
     const operator = operators.find((op) => op === value);
@@ -44,10 +118,10 @@ function App() {
         handleNumbers(value);
         break;
       case '.':
-        dotOperator(value);
+        dotOperator();
         break;
       case operator:
-        handleOperators();
+        handleOperators(value);
         break;
       default:
         break;
